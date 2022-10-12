897d6c4283f154a8c85d7ee63c82da5cde636fac
davgur <davgur.ru@gmail.com>
2022-10-12 13:59:19 +0300

    add test for broken json bug

---
+++ lib/chronicles/broken_json_test.go
@@ -0,0 +1,27 @@
+package chronicles
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/Bnei-Baruch/archive-my/common"
+)
+
+func (s *ChroniclesTestSuite) TestChronicles_brokenJson() {
+	user := s.CreateUser()
+
+	server := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		data := s.readTestResource("broken_json_test.json")
+		_, err := res.Write([]byte(fmt.Sprintf(string(data), user.AccountsID)))
+		s.Require().NoError(err)
+	}))
+	defer server.Close()
+	common.Config.ChroniclesUrl = server.URL
+
+	chr := new(Chronicles)
+	chr.InitWithDeps(s.MyDB.DB, s.MDB.DB)
+
+	_, err := chr.scanEvents()
+	s.Require().NoError(err)
+}
